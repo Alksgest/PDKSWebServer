b24a20fde708250e2c9651c33f6e0d4d587e83e0
vlky <vlky@PC-VLKY>
2019-09-27 09:45:07 +0300

    added mapper class

---
+++ PDKSWebServer/Mappers/ModelMapper.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using PDKSWebServer.Dtos;
+using PDKSWebServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDKSWebServer.Mappers
+{
+    internal class ModelMapper
+    {
+        private class TicksToDateTimeConverter : ITypeConverter<long, DateTime>
+        {
+            public DateTime Convert(long source, DateTime destination, ResolutionContext context)
+            {
+                DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                dtDateTime = dtDateTime.AddMilliseconds(source).ToLocalTime();
+                return dtDateTime;
+            }
+        }
+
+        private class DateTimeToTicksConverter : ITypeConverter<DateTime, long>
+        {
+            public long Convert(DateTime source, long destination, ResolutionContext context)
+            {
+                return new DateTimeOffset(source).ToUnixTimeMilliseconds();
+            }
+        }
+
+        private static ModelMapper _modelMapper;
+        internal static ModelMapper GetMapper =>
+            _modelMapper ?? (_modelMapper = new ModelMapper());
+
+        private readonly IMapper _mapper;
+
+        private ModelMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+
+                cfg.CreateMap<User, UserDto>();
+                cfg.CreateMap<UserDto, User>();
+            });
+
+            _mapper = new Mapper(config);
+        }
+
+        internal TDest Map<TSource, TDest>(TSource source)
+        {
+            if (source == null)
+                return default;
+            return _mapper.Map<TSource, TDest>(source);
+        }
+
+        internal List<TDest> MapList<TSource, TDest>(List<TSource> source)
+        {
+            if (source == null)
+                return null;
+
+            List<TDest> res = new List<TDest>();
+
+            foreach (var obj in source)
+            {
+                res.Add(Map<TSource, TDest>(obj));
+            }
+            return res;
+        }
+    }
+}
--- PDKSWebServer/Models/User.cs
+++ PDKSWebServer/Models/User.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static PDKSWebServer.Models.UserRole;
 
 namespace PDKSWebServer.Models
 {
@@ -10,6 +11,7 @@ namespace PDKSWebServer.Models
         public int ID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public UserRole Role { get; set; }
+        //public UserRole Role { get; set; }
+        public string Role { get; set; }
     }
 }
